08792f71a4a4353f43cf441313a99771f14005db
Pavol Rusnak <stick@gk2.sk>
2015-02-22 23:12:33 +0100

    symlink thanks.html to static dir to enable preview

--- server.py
+++ server.py
@@ -7,4 +7,4 @@ context.use_certificate_file('ca_ctjb.crt')
 from register import app
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port='4433', debug=False, ssl_context=context)
+	app.run(host='0.0.0.0', port=4433, debug=True, ssl_context=context)
