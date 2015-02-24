adf06cdc3a8f59dc13ea1f10c8410266e12ac178
Pavol Rusnak <stick@gk2.sk>
2015-02-24 13:15:45 +0100

    use new openssl api

--- server.py
+++ server.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
-from OpenSSL import SSL
-context = SSL.Context(SSL.SSLv23_METHOD)
-context.use_privatekey_file('ca_ctjb.key')
-context.use_certificate_file('ca_ctjb.crt')
-
 from register import app
+
+context = ('ca_ctjb.crt', 'ca_ctjb.key')
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=4433, debug=True, ssl_context=context)
