3ab28538d2462a664e1d70aec2b0e44d08784852
Pavol Rusnak <stick@gk2.sk>
2015-02-25 11:29:24 +0100

    use tornado

--- server.py
+++ server.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python
+from tornado.wsgi import WSGIContainer
+from tornado.ioloop import IOLoop
+from tornado.web import FallbackHandler, RequestHandler, Application
+from tornado.httpserver import HTTPServer
+
 from register import app
 
-context = ('ca_ctjb.crt', 'ca_ctjb.key')
+tr = WSGIContainer(app)
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=4433, debug=True, ssl_context=context)
+application = Application([
+	(r".*", FallbackHandler, dict(fallback=tr)),
+])
+
+http_server = HTTPServer(application, ssl_options={'certfile': 'ca_ctjb.crt', 'keyfile': 'ca_ctjb.key'})
+
+if __name__ == "__main__":
+	http_server.listen(4433, address='0.0.0.0')
+	IOLoop.instance().start()
