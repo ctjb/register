d0bcf6b3ee8ba4708ef0497721e6161e14d0e891
Pavol Rusnak <stick@gk2.sk>
2015-03-04 19:08:35 +0100

    update btc price

--- register/views.py
+++ register/views.py
@@ -56,7 +56,7 @@ def register():
 
 	user_id = person.id
 	price_czk = price * 28
-	price_btc = price / 200.0 + user_id * 0.00000001
+	price_btc = price / 250.0 + user_id * 0.00000001
 
 	msg = Message("CTJB 2015 Registracia", sender="ctjb@ctjb.net", recipients=[email])
 	msg.body = """
