09bf37cb7bdffa71110e4bf2963da648a962ce63
Pavol Rusnak <stick@gk2.sk>
2016-04-17 21:13:35 +0200

    more updates to 2016

--- server.py
+++ server.py
@@ -12,7 +12,12 @@ application = Application([
 	(r".*", FallbackHandler, dict(fallback=tr)),
 ])
 
-http_server = HTTPServer(application, ssl_options={'certfile': 'ca_ctjb.crt', 'keyfile': 'ca_ctjb.key'})
+try:
+    http_server = HTTPServer(application, ssl_options={'certfile': 'ca_ctjb.crt', 'keyfile': 'ca_ctjb.key'})
+    print('Running SSL server ...')
+except:
+    http_server = HTTPServer(application)
+    print('Running HTTP server ...')
 
 if __name__ == "__main__":
 	http_server.listen(4433, address='0.0.0.0')
