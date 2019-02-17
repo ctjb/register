02a6bc2a3c1179452f131f0750bfb5fd551e7069
Pavol Rusnak <pavol@rusnak.io>
2019-02-17 16:53:02 +0100

    add btc address to dump

--- webapp/db_dump.py
+++ webapp/db_dump.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 import sys
 
+import btc
 from main import db
 from models import Person
 
 paid_only = len(sys.argv) > 1 and sys.argv[1] == "paid"
+unpaid_only = len(sys.argv) > 1 and sys.argv[1] == "unpaid"
 
 for p in Person.query:
     if paid_only and not p.paid:
         continue
-    print(p.serialize())
+    if unpaid_only and p.paid:
+        continue
+    j = p.serialize()
+    j["btc"] = btc.addresses[p.id]
+    print(j)
